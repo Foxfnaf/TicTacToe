6a7172c6730a2ae235194532fc9e6d78016ce6df
Foxfnaf <foxyfnaf.93yt@gmail.com>
2024-09-08 13:51:57 +0200

    commit

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -67,92 +67,67 @@ public class TicTacToe {
     }
 
     public boolean win() {
-        if (board.get(0).equals("x") && board.get(1).equals("x") && board.get(2).equals("x")) {
-            System.out.println("Wygrałeś");
-            return true;
-        }
-
-        if (board.get(3).equals("x") && board.get(4).equals("x") && board.get(5).equals("x")) {
-            System.out.println("Wygrałeś");
-            return true;
-        }
-
-        if (board.get(6).equals("x") && board.get(7).equals("x") && board.get(8).equals("x")) {
-            System.out.println("Wygrałeś");
-            return true;
-        }
-
-        if (board.get(0).equals("x") && board.get(3).equals("x") && board.get(6).equals("x")) {
-            System.out.println("Wygrałeś");
-            return true;
-        }
-
-        if (board.get(1).equals("x") && board.get(4).equals("x") && board.get(7).equals("x")) {
-            System.out.println("Wygrałeś");
-            return true;
-        }
-
-        if (board.get(2).equals("x") && board.get(5).equals("x") && board.get(8).equals("x")) {
-            System.out.println("Wygrałeś");
-            return true;
-        }
-
-        if (board.get(0).equals("x") && board.get(4).equals("x") && board.get(8).equals("x")) {
-            System.out.println("Wygrałeś");
-            return true;
-        }
-
-        if (board.get(2).equals("x") && board.get(4).equals("x") && board.get(6).equals("x")) {
-            System.out.println("Wygrał");
-            return true;
-        }
-
-        if (board.get(0).equals("o") && board.get(1).equals("o") && board.get(2).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
-        if (board.get(3).equals("o") && board.get(4).equals("o") && board.get(5).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
-        if (board.get(6).equals("o") && board.get(7).equals("o") && board.get(8).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
-        if (board.get(0).equals("o") && board.get(3).equals("o") && board.get(6).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
-        if (board.get(1).equals("o") && board.get(4).equals("o") && board.get(7).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
-        if (board.get(2).equals("o") && board.get(5).equals("o") && board.get(8).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
-        if (board.get(0).equals("o") && board.get(4).equals("o") && board.get(8).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
-        if (board.get(2).equals("o") && board.get(4).equals("o") && board.get(6).equals("o")) {
-            System.out.println("Wygrał bot");
-            return true;
-        }
-
         int count = 0;
         for (int i = 0; i < board.size(); i++) {
-            if (board.get(i) == "x" || board.get(i) == "o") {
+            if (board.get(i).equals("x")) {
                 count++;
-                if (count == 9) {
-                    System.out.println("Pat");
+                if (count == 3){
+                    System.out.println("Wygrałeś");
+                    return true;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < board.size(); i += 3) {
+            if (board.get(i).equals("x")) {
+                count++;
+                if (count == 3){
+                    System.out.println("Wygrałeś");
+                    return true;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < board.size(); i += 4) {
+            if (board.get(i).equals("x")) {
+                count++;
+                if (count == 3){
+                    System.out.println("Wygrałeś");
+                    return true;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < board.size(); i++) {
+            if (board.get(i).equals("o")) {
+                count++;
+                if (count == 3){
+                    System.out.println("Wygrał bot");
+                    return true;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < board.size(); i += 3) {
+            if (board.get(i).equals("o")) {
+                count++;
+                if (count == 3){
+                    System.out.println("Wygrał bot");
+                    return true;
+                }
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < board.size(); i += 4) {
+            if (board.get(i).equals("o")) {
+                count++;
+                if (count == 3){
+                    System.out.println("Wygrał bot");
                     return true;
                 }
             }
